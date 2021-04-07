6e2baaa9040a8a02d05f565b9e163549bc44d0b6
Pedro Baptista <ze.pedro3622@outlook.pt>
2021-04-07 16:12:20 +0100

    Auth start

--- server/app.js
+++ server/app.js
@@ -3,9 +3,13 @@ const express = require("express");
 const userRouter = require("./routes/users");
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 const port = 3000;
-app.use("/api/users", userRouter);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
+
+app.use("/api/users", userRouter);
--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,11 +1,29 @@
 const fs = require("fs");
 
+const readJSONFile = (filePath) => {
+  const data = JSON.parse(fs.readFileSync(filePath));
+  return data;
+};
+
 const getAllUsers = (req, res) => {
-  const users = JSON.parse(fs.readFileSync("./data/users.json"));
-  console.log(JSON.stringify(users));
+  const users = readJSONFile("./data/users.json");
   return res.status(200).json(users);
+};
+
+const registerUser = (req, res) => {
+  console.log(req.body);
+  //   const { one_time_id, username } = req.body;
+  const users = readJSONFile("./data/users.json");
+  const result = users.filter((user) => {
+    return user.credentials === req.body;
+  });
+
+  //   console.log(users);
+  console.log(result);
+  return res.status(200).json({ success: result.length !== 0 });
 };
 
 module.exports = {
   getAllUsers,
+  registerUser,
 };
