456658261bc03ffae709a12f52656ae39c1469ad
Pedro Baptista <ze.pedro3622@outlook.pt>
2021-04-27 17:48:26 +0100

    2 new packages added; Register route finished

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,4 +1,5 @@
 const fs = require("fs");
+const jwt = require("jsonwebtoken");
 
 const readJSONFile = (filePath) => {
   const data = JSON.parse(fs.readFileSync(filePath));
@@ -11,16 +12,32 @@ const getAllUsers = (req, res) => {
 };
 
 const registerUser = (req, res) => {
-  console.log(req.body);
-  //   const { one_time_id, username } = req.body;
-  const users = readJSONFile("./data/users.json");
-  const result = users.filter((user) => {
-    return user.credentials === req.body;
-  });
+  const { one_time_id, username } = req.body;
 
-  //   console.log(users);
-  console.log(result);
-  return res.status(200).json({ success: result.length !== 0 });
+  const users = readJSONFile("./data/users.json");
+
+  // Search for user
+  const user_match = users.find(
+    (user) =>
+      user.credentials.one_time_id === one_time_id &&
+      user.credentials.username === username
+  );
+
+  // (one_time_id, username) pair found
+  if (user_match) {
+    // Creates JWT
+    const auth_token = jwt.sign(
+      { username: user_match.credentials.username },
+      process.env.JWT_SECRET,
+      { expiresIn: "1h" }
+    );
+
+    // Returns JWT
+    return res.status(200).json({ token: auth_token });
+  }
+
+  // Registration failed
+  return res.status(400).json({ error: "Invalid credentials!" });
 };
 
 module.exports = {
