b646a02f35a6db6fbe9e83de3c2d376a9a2616ea
zepedr0 <zepedromm1998@gmail.com>
2021-04-28 19:56:26 +0100

    Storing/Retrieval of msgs + En/Decryption and Signature Sign/Check

---
+++ client/test.js
@@ -0,0 +1,34 @@
+var fs = require("fs");
+const Cryptography = require('./utils/cryptography');
+const Messages = require('./utils/messages');
+
+const one_time_id = '45a78b4f3456';
+const message = 'An organization intends to design and implement a system with a trusted server supplying services to client applications, used by their collaborators.';
+const private_key = fs.readFileSync('./data/45a78b4f3456/keys/privateKey.pem', 'utf-8');
+const public_key = fs.readFileSync('./data/45a78b4f3456/keys/publicKey.pem', 'utf-8');
+
+// Encrypt
+const encryptedMsg = Cryptography.encrypt(public_key, message);
+
+// Sign
+const signature = Cryptography.sign(private_key, encryptedMsg);
+
+// Store Msg
+Messages.storeMessage(one_time_id, encryptedMsg, signature);
+
+// Get Messages
+const msgs = Messages.getMessages(one_time_id);
+
+// Decrypt Messages and check signature
+msgs.forEach(msg => {
+    console.log('\n\n######################')
+    console.log('Encrypted Message:', msg.msg);
+    console.log('Signature:', msg.signature);
+
+    console.log();
+    
+    const decryptedMsg = Cryptography.decrypt(private_key, msg.msg);
+    const signatureCheck = Cryptography.checkSign(public_key, msg.msg, msg.signature);
+    console.log('Decrypted Message: ', decryptedMsg);
+    console.log('Signature Check: ', signatureCheck);
+});
---
+++ client/utils/cryptography.js
@@ -0,0 +1,54 @@
+const crypto = require("crypto");
+
+// Encrypt message
+const encrypt = (publicKey, msg) => {
+  const encryptedData = crypto.publicEncrypt(
+    {
+      key: publicKey,
+      padding: crypto.constants.RSA_PKCS1_OAEP_PADDING,
+      oaepHash: "sha256",
+    },
+    Buffer.from(msg)
+  );
+
+  return encryptedData;
+};
+
+// Decrypt message
+const decrypt = (privateKey, msg) => {
+  const decryptedData = crypto.privateDecrypt(
+    {
+      key: privateKey,
+      padding: crypto.constants.RSA_PKCS1_OAEP_PADDING,
+      oaepHash: "sha256",
+    },
+    Buffer.from(msg)
+  );
+
+  return decryptedData.toString();
+};
+
+// Generate signature
+const sign = (privateKey, msg) => {
+  const signer = crypto.createSign("RSA-SHA256");
+  signer.write(Buffer.from(msg));
+  signer.end();
+
+  const signature = signer.sign(privateKey, "base64");
+  return signature;
+};
+
+// Verify signature
+const checkSign = (publicKey, msg, signature) => {
+  const verifier = crypto.createVerify("RSA-SHA256");
+  verifier.update(Buffer.from(msg));
+  const ver = verifier.verify(publicKey, signature, "base64");
+  return ver;
+};
+
+module.exports = {
+  encrypt,
+  decrypt,
+  sign,
+  checkSign,
+};
